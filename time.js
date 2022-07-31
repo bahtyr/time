929527195808a6ad0a4208ee508f01750ca9d2b3
Bahtiyar Ceylan <50676218+bahtyr@users.noreply.github.com>
2022-07-31 11:09:46 -0400

    init

---
+++ time.js
@@ -0,0 +1,77 @@
+var topics = [];
+
+class Topic {
+	position;
+	el;
+
+	name;
+	duration = 0;
+	color;
+	isActive = false;
+
+	constructor(name, color, pos) {
+		this.name = name;
+		this.color = color;
+		this.position = pos;
+		this.printSelf();
+	}
+
+	printSelf() {
+		this.el = document.createElement('div');
+		this.el.classList.add("topic");
+		this.el.setAttribute("onclick", `topics[${this.position}].click()`);
+		this.el.style = `--data-color: ${this.color};`;
+		this.el.innerHTML = `<p class="topic-name">${this.name}</p><p class="topic-duration"></p>`;
+		document.body.appendChild(this.el);
+	}
+
+	click() {
+		if (this.isActive) this.stop();
+		else this.start();
+	}
+	
+	start() {
+		this.el.classList.add("active");
+		this.isActive = true;
+		setTimeout(this.count.bind(this), 100);
+	}
+
+	stop() {
+		this.el.classList.remove("active");
+		this.isActive = false
+	}
+
+	count() {
+		this.duration += 1;
+		if (this.isActive) 
+			setTimeout(this.count.bind(this), 100);
+		this.el.children[1].textContent = this.duration;
+	}
+}
+
+
+var colors = [
+	"hsla(271, 70%, 40%, 1)",
+	"hsla(271, 99%, 40%, 1)",
+	"hsla(272, 85%, 50%, 1)",
+	"hsla(202, 71%, 32%, 1)",
+	"hsla(145, 63%, 32%, 1)",
+	"hsla(29, 47%, 50%, 1)",
+	"hsla(29, 100%, 46%, .5)",
+	"hsla(324, 100%, 40%, .5)",
+	"hsla(0, 0%, 20%, .5)",
+];
+
+function randomColor() {
+	let i = Math.floor(Math.random() * colors.length);
+	let c = colors[i];
+	colors.splice(i, 1);
+	return c;
+}
+
+topics.push(new Topic("DB",       colors[5], topics.length));
+topics.push(new Topic("UI",       colors[7], topics.length));
+topics.push(new Topic("API",      colors[3], topics.length));
+topics.push(new Topic("JavaScript", colors[8], topics.length));
+topics.push(new Topic("HTML",     colors[8], topics.length));
+topics.push(new Topic("CSS",     colors[8], topics.length));
