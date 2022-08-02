c5fcc95f651c91cc9f8f0e78ad6db711f6f6877b
Bahtiyar Ceylan <50676218+bahtyr@users.noreply.github.com>
2022-08-01 22:36:27 -0400

    use canvas for today's progress

--- canvas.js
+++ canvas.js
@@ -5,23 +5,27 @@ class Canvas {
 	ctx;
 	size;
 
-	constructor(id) {
+	offset = 0;
+	allowNewLine = true;
+
+	constructor(id, height) {
 		this.canvas = document.getElementById(id);
 		this.ctx = this.canvas.getContext("2d");
 
 		let scale = window.devicePixelRatio;
 		this.size = this.canvas.parentElement.offsetWidth;
 		this.canvas.style.width  = `${this.size}px`;
-		this.canvas.style.height = `${400}px`;
+		this.canvas.style.height = `${height}px`;
 		this.canvas.width  = Math.floor(this.size * scale);
-		this.canvas.height = Math.floor(400 * scale);
+		this.canvas.height = Math.floor(height * scale);
 		this.ctx.scale(scale, scale);
 	}
 
 	draw(color) {
-		let x = (this.shape.width + this.shape.gapX) * this.pos.i;
+		let x = this.offset + (this.shape.width + this.shape.gapX) * this.pos.i;
 
 		if (x + this.shape.width > this.size) {
+			if (!this.allowNewLine) return;
 			this.pos.row++;
 			this.pos.i = 0;
 			x = 0;
@@ -35,4 +39,4 @@ class Canvas {
 	}
 }
 
-const canvas = new Canvas("canvas-main");
+const canvas = new Canvas("canvas-main", 400);
--- today.js
+++ today.js
@@ -2,35 +2,41 @@
  * Today's Date
  ************************************************/
 
-const el = document.getElementsByTagName("h2")[0];
-const options = { weekday: 'long', month: 'numeric', day: '2-digit' };
-const date = new Date().toLocaleDateString("en-US", options).split(", ");
-el.innerHTML = `${date[0]}<span> ${date[1]}</span>`;
+const el = document.getElementsByTagName("h4")[0];
+const options = {weekday: 'short'};
+el.innerHTML = new Date().toLocaleDateString("en-US", options);
 
 /************************************************
- * Progress Bar
+ * Progress
  ************************************************/
 
-class Progress {
-	t = setInterval(this.calc, 15 * 60 * 1000);
-	bar1 = document.getElementById("bar1");
-	bar2 = document.getElementById("bar2");
+const canvasToday = new Canvas("canvas-today", 40);
 
-	constructor() {
-		this.calc()
-	}
+canvasToday.shape = { width: 3, height: 25, gapX: 6, gapY: 0};
+canvasToday.offset = 46 + 4;
+canvasToday.allowNewLine = false;
+canvasToday.t = setInterval(this.print, 15 * 60 * 1000);
 
-	calc() {
-		let d = new Date();
-		let now = (d.getHours() * 60) + d.getMinutes();
-		let total = 24 * 60;
-		// percentage
-		let percentage = Math.floor((now / total) * 100);
-		let percentage2 = 100 - percentage;
-		// set
-		this.bar1.style.width = percentage + "%";
-		this.bar2.style.width = percentage2 + "%";
+canvasToday.print = function() {
+	//calculate day progress
+	let d = new Date();
+	let hrsNow = (d.getHours() * 60) + d.getMinutes();
+	let hrsTotal = 24 * 60;
+	let shpTotal = this.pos.i; //get last item count
+
+	let highlightCount = Math.floor((hrsNow * shpTotal) / hrsTotal);
+	let loop = shpTotal > 0 ? shpTotal : 200;
+
+	//reset to draw over
+	this.pos.i = 0;
+
+	//print
+	for (let i = 0; i < loop; i++) {
+		if (i < highlightCount)
+			this.draw("hsl(0, 0%, 27%)");
+		else this.draw("hsl(0, 0%, 12%)");
 	}
 }
 
-new Progress();
+canvasToday.print(); // learn max shape count (total)
+canvasToday.print(); // highlight time
