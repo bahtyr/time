aea564107e5fa4c202a4ed013352216138527a50
Bahtiyar Ceylan <50676218+bahtyr@users.noreply.github.com>
2022-08-07 10:18:26 -0400

    add Clear button

--- edit.js
+++ edit.js
@@ -1,11 +1,26 @@
+const btnClear = document.getElementById("btn-clear");
 const btnNew = document.getElementById("btn-new");
 const modalNew = new Modal("modal-new");
 modalNew.inputTitle = document.getElementById("input-new-title");
 modalNew.save = document.getElementById("btn-new-save");
+
 modalNew.save.addEventListener("click", e => {
 	let title = modalNew.inputTitle.value.trim();
 	topics.push(new Topic(title, randomColor(), topics.length));
 	saveData();
 	modalNew.hide()
 });
-btnNew.addEventListener("click", e => modalNew.show());
+
+btnNew.addEventListener("click", e => {
+	modalNew.show();
+	modalNew.inputTitle.focus();
+});
+
+btnClear.addEventListener("click", e => {
+	for (let i in topics) {
+		clearTimeout(topics[i].interval)
+		topics[i].el.remove();
+	}
+	topics = [];
+	saveData();
+});
